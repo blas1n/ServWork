5b4a7a52650325c3c3f5d461f41c8a80940fd89a
blAs1N <qazasa123@gmail.com>
2019-12-25 19:39:07 +0900

    Fix return type warning using SFINAE

--- server/base_library/ThreadPool.h
+++ server/base_library/ThreadPool.h
@@ -10,13 +10,42 @@
 
 class ThreadPool final
 {
+private:
+	template <class RetType>
+	using TaskRetType = std::enable_if_t<!std::is_void_v<RetType>, RetType>;
+
 public:
 	ThreadPool();
     ~ThreadPool();
 
 	template <class Fn, class... Args>
-	std::future<std::invoke_result_t<Fn, Args...>> AddTask(
-		Fn&& fn, Args&&... args) noexcept;
+	std::future<TaskRetType<std::invoke_result_t<Fn, Args...>>>
+		AddTask(Fn&& fn, Args&&... args) noexcept
+	{
+		auto task = std::make_shared<
+			std::packaged_task<std::invoke_result_t<Fn, Args...>()>>(
+				std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...)
+				);
+
+		taskMutex.lock();
+		tasks.push([task] { (*task)(); });
+		taskMutex.unlock();
+
+		cv.notify_one();
+		return task->get_future();
+	}
+
+	template <class Fn, class... Args>
+	void AddTask(Fn&& fn, Args&&... args) noexcept
+	{
+		auto task = std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...);
+
+		taskMutex.lock();
+		tasks.push([task = std::move(task)]{ task(); });
+		taskMutex.unlock();
+
+		cv.notify_one();
+	}
 
 private:
 	void ThreadWork() noexcept;
@@ -28,19 +57,3 @@ private:
 	std::mutex taskMutex;
 	bool isEnd;
 };
-
-template <class Fn, class... Args>
-std::future<std::invoke_result_t<Fn, Args...>> ThreadPool::AddTask(Fn&& fn, Args&&... args) noexcept
-{
-	auto task = std::make_shared<
-		std::packaged_task<std::invoke_result_t<Fn, Args...>()>>(
-			std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...)
-	);
-
-	taskMutex.lock();
-	tasks.push([task] { (*task)(); });
-	taskMutex.unlock();
-
-	cv.notify_one();
-	return task->get_future();
-}
