6e923273b21a485d645a28cfe4d9eb8e24fe34fe
blAs1N <qazasa123@gmail.com>
2019-12-26 09:12:02 +0900

    Change cmake macro

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -4,19 +4,30 @@ macro (set_project)
 endmacro ()
 
 macro (set_public_dependencies)
-	target_link_libraries (${PROJECT_ID} PUBLIC ${ARGN})
-	get_filename_component(PARENT_DIR ${CMAKE_CURRENT_SOURCE_DIR} DIRECTORY)
-
-	foreach (ARG ${ARGN})
-		target_include_directories (${PROJECT_ID} PUBLIC ${PARENT_DIR}/${ARG})
-	endforeach ()
+	set_dependencies_internal(PUBLIC ${ARGN})
 endmacro ()
 
 macro (set_private_dependencies)
-	target_link_libraries (${PROJECT_ID} PRIVATE ${ARGN})
+	set_dependencies_internal(PRIVATE ${ARGN})
+endmacro ()
+
+macro (set_dependencies_internal QUALIFIER)
 	get_filename_component(PARENT_DIR ${CMAKE_CURRENT_SOURCE_DIR} DIRECTORY)
 
 	foreach (ARG ${ARGN})
-		target_include_directories (${PROJECT_ID} PRIVATE ${PARENT_DIR}/${ARG})
+		string(REGEX REPLACE "./" "" TARGET_NAME ${ARG})
+		target_link_libraries (${PROJECT_ID} ${QUALIFIER} ${TARGET_NAME})
+
+		set (ABS_PATH ${PARENT_DIR}/${ARG})
+
+		if (NOT EXISTS ${ABS_PATH})
+			set (ABS_PATH ${CMAKE_SOURCE_DIR}/${ARG})
+		endif()
+
+		if (NOT EXISTS ${ABS_PATH})
+				message (FATAL_ERROR "${ABS_PATH} does not exists.")
+		endif ()
+
+		target_include_directories (${PROJECT_ID} ${QUALIFIER} ${ABS_PATH})
 	endforeach ()
 endmacro ()
