61e9e0cdf84ceaed0016f07b99fbe44278903e15
blAs1N <qazasa123@gmail.com>
2019-12-28 11:25:46 +0900

    Wrap namespace

--- server/base_library/ThreadPool.cpp
+++ server/base_library/ThreadPool.cpp
@@ -2,39 +2,42 @@
 #include <exception>
 #include <utility>
 
-ThreadPool::ThreadPool()
-	: threads(), tasks(), cv(), taskMutex(), isEnd(false)
+namespace ServWork
 {
-	auto threadNum = std::thread::hardware_concurrency();
-	if (threadNum == 0)
-		throw std::runtime_error{ "Hardware Concurrency is 0" };
-
-	threads.reserve(threadNum);
-
-	while (threadNum--)
-		threads.emplace_back([this]() { ThreadWork(); });
-}
-
-ThreadPool::~ThreadPool()
-{
-	isEnd = true;
-	cv.notify_all();
-
-	for (auto& t : threads)
-		t.join();
-}
-
-void ThreadPool::ThreadWork() noexcept
-{
-	while (true)
+	ThreadPool::ThreadPool()
+		: threads(), tasks(), cv(), taskMutex(), isEnd(false)
 	{
-		std::unique_lock<std::mutex> lock{ taskMutex };
-		cv.wait(lock, [this]() { return !tasks.empty() || isEnd; });
-		if (isEnd && tasks.empty()) return;
+		auto threadNum = std::thread::hardware_concurrency();
+		if (threadNum == 0)
+			throw std::runtime_error{ "Hardware Concurrency is 0" };
 
-		auto&& task = std::move(tasks.front());
-		tasks.pop();
-		lock.unlock();
-		task();
+		threads.reserve(threadNum);
+
+		while (threadNum--)
+			threads.emplace_back([this]() { ThreadWork(); });
+	}
+
+	ThreadPool::~ThreadPool()
+	{
+		isEnd = true;
+		cv.notify_all();
+
+		for (auto& t : threads)
+			t.join();
+	}
+
+	void ThreadPool::ThreadWork() noexcept
+	{
+		while (true)
+		{
+			std::unique_lock<std::mutex> lock{ taskMutex };
+			cv.wait(lock, [this]() { return !tasks.empty() || isEnd; });
+			if (isEnd && tasks.empty()) return;
+
+			auto&& task = std::move(tasks.front());
+			tasks.pop();
+			lock.unlock();
+			task();
+		}
 	}
 }
