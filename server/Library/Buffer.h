04195107e179e2d2e553ede7672574a16394ede1
blAs1N <qazasa123@gmail.com>
2019-12-30 15:57:34 +0900

    Rename base_library to Library

--- server/Library/Buffer.h
+++ server/Library/Buffer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstring>
+#include <stdexcept>
 
 namespace ServWork
 {
---
+++ server/Library/Core.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstddef>
+#include <stdexcept>
+
+namespace ServWork
+{
+	using LogicError = std::logic_error;
+	using RuntimeError = std::runtime_error;
+
+	using byte = unsigned char;
+
+	using uint8 = unsigned char;
+	using uint16 = unsigned short;
+	using uint32 = unsigned int;
+	using uint64 = unsigned long long;
+
+	using int8 = signed char;
+	using int16 = signed short;
+	using int32 = signed int;
+	using int64 = signed long long;
+	
+#pragma pack(push, 1)
+	struct Header final
+	{
+		uint8 check;
+		uint8 id;
+		uint32 size;
+	};
+#pragma pack(pop)
+
+	constexpr auto HEADER_SIZE = sizeof(Header);
+}
