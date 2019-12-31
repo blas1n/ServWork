e985f4aa88b5889c943d6c4c6d8f3ff6c430c4cc
blAs1N <qazasa123@gmail.com>
2019-12-31 14:50:11 +0900

    Fix include error

--- server/Socket/UserData.h
+++ server/Socket/UserData.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Def.h"
+#include "BaseSocket.h"
 
 namespace ServWork
 {
@@ -11,7 +12,7 @@ namespace ServWork
 		~UserData();
 
 		inline const BaseSocket& GetSocket() const noexcept { return *sock; }
-		inline void SetSocket(const BaseSocket& inSock) noexcept { (*sock) = inSock; }
+		inline void SetSocket(const BaseSocket& inSock) noexcept { *sock = inSock; }
 
 		inline const Sender* GetSender() const noexcept { return sender; }
 		inline const Receiver* GetReceiver() const noexcept { return receiver; }
@@ -20,7 +21,7 @@ namespace ServWork
 		inline void SetIp(const wchar_t* inIp) noexcept { wcsncpy(ip, inIp, 16); }
 
 	protected:
-		class BaseSocket* sock;
+		BaseSocket* sock;
 		Sender* sender;
 		Receiver* receiver;
 		wchar_t ip[16];
